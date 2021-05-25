d277e2f35a0bffef8b0f4e22e13d497540363285
karthikeyanvijay <vijayanand.karthikeyan@gmail.com>
2021-05-25 22:50:54 +0000

    Update for export/import CDE jobs

--- export-import-cde-jobs/cde-vc-jobs-export.sh
+++ export-import-cde-jobs/cde-vc-jobs-export.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <parameter-file>"
+# Sample Usage: cde-vc-jobs-export.sh <source-cluster-name> <source-vc-name> <workload-username>
+# Description: 
+#   This script exports the jobs from the Virtual Cluster.
+#   Script assumes that the CDE cli is already setup with the right access.
+#   When prompted, user should enter their workload password
+# Author: Vijay Anand Karthikeyan
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <source-cluster-name> <source-vc-name> <workload-username>"
     exit 1
 fi
-source $1
-#### Get token
-export SOURCE_TOKEN=`curl --user ${username}  -k https://$SOURCE_SVC/gateway/authtkn/knoxtoken/api/v1/token | jq -r .access_token`
 
-#### Export All Jobs
-curl -H "Authorization: Bearer ${SOURCE_TOKEN}" -k -X GET "https://$SOURCE_VC/dex/api/v1/admin/export?exportjobs=true&exportjobresources=true&exportresources=false" -H "accept: application/zip" --output ${BACKUP_FILE_PREFIX}_source_export.zip
+SOURCE_CLUSTER_NAME=$1
+SOURCE_VC_NAME=$2
+USERNAME=$3
+SOURCE_VC_EXPORT_FILE_NAME=${SOURCE_CLUSTER_NAME}-${SOURCE_VC_NAME}.zip
+
+# Derive the required parameters from the input
+SOURCE_CLUSTER_ID=$(cdp de list-services | jq -r '.services[] | select( .name == '\"$SOURCE_CLUSTER_NAME\"' and .status == "ClusterCreationCompleted") | .clusterId')
+SOURCE_VC_ID=$(cdp de list-vcs --cluster-id $SOURCE_CLUSTER_ID | jq -r '.vcs[] | select( .vcName == '\"$SOURCE_VC_NAME\"' and .clusterId == '\"$SOURCE_CLUSTER_ID\"' and .status == "AppInstalled") | .vcId')
+SOURCE_VCAPIURL=$(cdp de describe-vc --cluster-id $SOURCE_CLUSTER_ID --vc-id $SOURCE_VC_ID | jq -r '.vc | select( .vcName == '\"$SOURCE_VC_NAME\"' and .clusterId == '\"$SOURCE_CLUSTER_ID\"' and .status == "AppInstalled") | .vcApiUrl')
+SOURCE_DOMAIN=$(echo $SOURCE_VCAPIURL | awk -F[/:] '{print $4}' | awk -F. '{ for (i=2; i<=NF; i++) printf "."$i }')
+SOURCE_SERVICE="service"$SOURCE_DOMAIN
+
+echo "Source Cluster ID: $SOURCE_CLUSTER_ID, Name: $SOURCE_CLUSTER_NAME"
+echo "Source VC      ID: $SOURCE_VC_ID, Name: $SOURCE_VC_NAME"
+echo "Source VC API URL: $SOURCE_VCAPIURL"
+echo ""
+
+# Get token
+SOURCE_TOKEN=$(curl -s --user ${USERNAME} -k https://$SOURCE_SERVICE/gateway/authtkn/knoxtoken/api/v1/token | jq -r .access_token)
+
+# Export jobs
+curl -s -H "Authorization: Bearer ${SOURCE_TOKEN}" -k -X GET "$SOURCE_VCAPIURL/admin/export?exportjobs=true&exportjobresources=true&exportresources=false" -H "accept: application/zip" --output ${SOURCE_VC_EXPORT_FILE_NAME}
+if [ $? -eq 0 ]; then
+    echo "Jobs from the CDE service ${SOURCE_CLUSTER_NAME}, virtual cluster ${SOURCE_VC_NAME} exported to ${SOURCE_VC_EXPORT_FILE_NAME}"
+else
+    echo "Job export failed"
+fi
--- export-import-cde-jobs/cde-vc-jobs-import.sh
+++ export-import-cde-jobs/cde-vc-jobs-import.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <parameter-file>"
+# Sample Usage: cde-vc-jobs-import.sh <target-cluster-name> <target-vc-name> <workload-username> <file-name>
+# Description: 
+#   This script imports the jobs to the Virtual Cluster.
+#   Script assumes that the CDE cli is already setup with the right access.
+#   When prompted, user should enter their workload password
+# Author: Vijay Anand Karthikeyan
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <target-cluster-name> <target-vc-name> <workload-username> <file-name>"
     exit 1
 fi
-source $1
-#### Import to Target service
-#### Get token
-export TARGET_TOKEN=`curl --user ${username}  -k https://$TARGET_SVC/gateway/authtkn/knoxtoken/api/v1/token | jq -r .access_token`
+
+TARGET_CLUSTER_NAME=$1
+TARGET_VC_NAME=$2
+USERNAME=$3
+FILE_NAME=$4
+
+# Derive the required parameters from the input
+TARGET_CLUSTER_ID=$(cdp de list-services | jq -r '.services[] | select( .name == '\"$TARGET_CLUSTER_NAME\"' and .status == "ClusterCreationCompleted") | .clusterId')
+TARGET_VC_ID=$(cdp de list-vcs --cluster-id $TARGET_CLUSTER_ID | jq -r '.vcs[] | select( .vcName == '\"$TARGET_VC_NAME\"' and .clusterId == '\"$TARGET_CLUSTER_ID\"' and .status == "AppInstalled") | .vcId')
+TARGET_VCAPIURL=$(cdp de describe-vc --cluster-id $TARGET_CLUSTER_ID --vc-id $TARGET_VC_ID | jq -r '.vc | select( .vcName == '\"$TARGET_VC_NAME\"' and .clusterId == '\"$TARGET_CLUSTER_ID\"' and .status == "AppInstalled") | .vcApiUrl')
+TARGET_DOMAIN=$(echo $TARGET_VCAPIURL | awk -F[/:] '{print $4}' | awk -F. '{ for (i=2; i<=NF; i++) printf "."$i }')
+TARGET_SERVICE="service"$TARGET_DOMAIN
+
+echo "Target Cluster ID: $TARGET_CLUSTER_ID, Name: $TARGET_CLUSTER_NAME"
+echo "Target VC      ID: $TARGET_VC_ID, Name: $TARGET_VC_NAME"
+echo "Target VC API URL: $TARGET_VCAPIURL"
+echo ""
+
+# Get token
+TARGET_TOKEN=$(curl -s --user ${USERNAME} -k https://$TARGET_SERVICE/gateway/authtkn/knoxtoken/api/v1/token | jq -r .access_token)
+
 #### Import jobs
-curl -k -H "Authorization: Bearer ${TARGET_TOKEN}" -X POST "https://$TARGET_VC/dex/api/v1/admin/import" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./${BACKUP_FILE_NAME};type=application/zip" -F duplicatehandling=error
+curl -k -H "Authorization: Bearer ${TARGET_TOKEN}" -X POST "$TARGET_VCAPIURL/admin/import" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@./${FILE_NAME};type=application/zip" -F duplicatehandling=error
